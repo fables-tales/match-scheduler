e9b5f82d6842c2888472e34c4de272d718310900
Sam Phippen <samphippen@googlemail.com>
2010-04-12 17:42:38 +0100

    Convert to using objects for both teams and matches

--- matchchooser.py
+++ matchchooser.py
@@ -2,12 +2,91 @@
 import sys
 import random
 
+class Team:
+	def __init__(self, number):
+		self.number = number
+		self.matches = []
+
+	def get_number_of_matches(self):
+		return len(self.matches)
+
+	def get_matches(self):
+		r = ""
+		for i in self.matches:
+			r += str(i._number) + " "
+
+		return r
+
+class Match:
+	def __init__(self, number):
+		self._number = number
+		self._teams = []
+
+	def add_team(self, team):
+		self._teams.append(team)
+		team.matches.append(self)
+
+	def get_number_of_teams(self):
+		return len(self._teams)
+
+	def __str__(self):
+		return str(self._number)
+
+	def __unicode__(self):
+		return self.__str__()
+
+	def teams(self):
+		r = ""
+
+		for t in self._teams:
+			r += str(t.number) + " "
+
+		return r
+
+
 def gcd(a, b):
 	if b == 0:
 		return a
 	else:
 		return gcd(b, a%b)
 
+def max_matches_per_team(desired_matches, teams, slots_per_match):
+	return (desired_matches*slots_per_match) / teams
+
+def setup_matches(desired_matches, number_of_teams, slots_per_match):
+	teams = []
+
+	for i in range(1, number_of_teams+1):
+		t = Team(i)
+		teams.append(t)
+
+	tlist = teams[0:]
+
+	max_matches = max_matches_per_team(desired_matches, number_of_teams, slots_per_match)
+	matches = []
+
+	for i in range(0, desired_matches):
+		match = Match(i+1)
+		random.shuffle(tlist)
+
+		while match.get_number_of_teams() < slots_per_match:
+			#get a choice we can actually use
+			choice = random.choice(tlist)
+
+			while (choice in match._teams):
+				choice = random.choice(tlist)
+
+			tlist.remove(choice)
+
+			match.add_team(choice)
+
+			#reinitialise teams if we've used them all
+			if len(tlist) == 0:
+				tlist = teams[0:]
+
+		matches.append(match)
+
+	return matches, teams
 
 if __name__ == "__main__":
 
@@ -35,41 +114,22 @@ if __name__ == "__main__":
 		print (slots/baseMatches+1)*baseMatches/teamsPerMatch, "matches"
 		print "continuing with unfair number of matches"
 
-	matches = [None]*desiredMatches
-	for i in range(0,teams):
-		teamsmatches.append([])
+	matches, team_o = setup_matches(desiredMatches, teams, teamsPerMatch)
 
-	teamindex = random.randint(0, teams -1)
-
-	tlist = range(1, teams+1)
-	for i in range(0, desiredMatches):
-		match = []
-		while len(match) < teamsPerMatch:
-			choice = random.choice(tlist)
-			tlist.remove(choice)
-			match.append(choice)
-			teamsmatches[choice-1].append(i+1)
-
-			if tlist == []:
-				tlist = range(1,teams+1)
-
-		matches[i] = match
-
-	for i in range(0, teams):
-		print "Team %d is in %d matches: %s" % (i+1, len(teamsmatches[i]), teamsmatches[i])
-
-	for i in range(0, desiredMatches):
-		print "Match number: %d" % (i + 1)
-		print "Teams:", matches[i]
+	for i in team_o:
+		print "Team %d in %d matches:" % (i.number, i.get_number_of_matches()), i.get_matches()
+#	random.shuffle(matches)
+	for m in matches:
+		print "Match %d:" % m._number, m.teams()
 
 	print "writing matches to matches.csv"
 	csv = open("matches.csv", "w")
 
 	for match in matches:
-		for i in range(0, len(match)):
-			csv.write(str(match[i]))
+		for i in range(0, len(match._teams)):
+			csv.write(str(match._teams[i].number))
 
-			if i != len(match)-1:
+			if i != len(match._teams)-1:
 				csv.write(",")
 			else:
 				csv.write("\n")
