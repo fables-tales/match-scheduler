cd509b9f76a57be24a544d25b2a155fc0ccb7012
sam <sam@ninjacow.xen.prgmr.com>
2010-04-14 21:49:31 +0000

    prettyprint blues

--- blues.py
+++ blues.py
@@ -13,7 +13,7 @@ def generateblocks(width, height, rampleft, ramptop, rampwidth, rampheight, blue
         for j in range(0, height):
             
             if (not_in_rectangle(rampleft, ramptop, rampwidth, rampheight, i, j)):
-                positions.append((i+1,j+1))
+                positions.append((i,j))
                 
     blue_blocks = []
     for i in range(0, blues):
@@ -23,5 +23,26 @@ def generateblocks(width, height, rampleft, ramptop, rampwidth, rampheight, blue
 
 if __name__ == "__main__":
     blues = generateblocks(7, 7, 2, 3, 3, 1)
+    print "+--------------+"
+    chars = []
+    for j in range(0, 7):
+        chars.append([])
+        for i in range(0, 7):
+            if (i,j) in blues:
+                chars[j].append("B")
+            elif (j == 3 and (i == 2 or i == 3 or i == 4)):
+                chars[j].append("R")
+            else:
+                chars[j].append(".")
+    
+    
+    for row in chars:
+        r = ""
+        for c in row:
+            r += c + " "
+        print "| "+r+"|"
+    print "+--------------+"
+                
+        
     print blues
 
