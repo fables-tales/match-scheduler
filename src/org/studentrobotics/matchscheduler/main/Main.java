0475fef4ea1e48c6b54e6c743526ad24afe27700
KE Kerry <kk8599@bris.ac.uk>
2010-11-17 18:35:24 +0000

    changed match schedules to accept command land arguments
    This is so it is not needed to recompile if the factors need changing

--- src/org/studentrobotics/matchscheduler/main/Main.java
+++ src/org/studentrobotics/matchscheduler/main/Main.java
@@ -14,34 +14,62 @@ import org.studentrobotics.matchscheduler.serializers.PrintMatchesSerializer;
 import org.studentrobotics.matchscheduler.serializers.TeamSerializer;
 
 public class Main {
-	private static List<MatchSerializer> serializers = new ArrayList<MatchSerializer>();
+    private static List<MatchSerializer> serializers = new ArrayList<MatchSerializer>();
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// add some serializers
-		serializers.add(new PrintMatchesSerializer());
-		serializers.add(new TeamSerializer());
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        if(args.length!=6){
+            System.out.println("Wrong number of arguments");
+            System.exit(1);
+        }
+        
+        else {
+        
+            // add some serializers
+            serializers.add(new PrintMatchesSerializer());
+            serializers.add(new TeamSerializer());
 
-		// setup match constraints
-		MatchConstraints mc = new MatchConstraints();
-		mc.setNumberOfTeams(13);
-		mc.setNumberOfMatches(23);
-		mc.setTeamsPerMatch(4);
-		mc.enableByes();
-		mc.setMinByeSize(2);
-		mc.setMaxByeSize(3);
+            int num_teams = Integer.parseInt(args[0]);
+            int num_matches = Integer.parseInt(args[1]);
+            int match_size = Integer.parseInt(args[2]);
+            String byes = args[3];
+            int min_byes = Integer.parseInt(args[4]);
+            int max_byes = Integer.parseInt(args[5]);
 
-		MatchSchedulerImpl ms = new MatchSchedulerImpl(new MaxSizeByeResolutionStrategy());
-		List<Match> matches = ms.schedule(mc);
+            // setup match constraints
+            MatchConstraints mc = new MatchConstraints();
 
-		List<Match> finalMatches = MatchAnnealer.anneal(matches, 30);
+            mc.setNumberOfTeams(num_teams);
+            mc.setNumberOfMatches(num_matches);
+            mc.setTeamsPerMatch(match_size);
+            if(byes=="true"){
+                mc.enableByes();
+                mc.setMinByeSize(min_byes);
+                mc.setMaxByeSize(max_byes);
+            }
 
-		for (MatchSerializer serializer : serializers) {
-			serializer.serialize(finalMatches, Team.generateTeamList(finalMatches));
-		}
+            /*mc.setNumberOfTeams(13);
+            mc.setNumberOfMatches(23);
+            mc.setTeamsPerMatch(4);
+            mc.enableByes();
+            mc.setMinByeSize(2);
+            mc.setMaxByeSize(3);
+            */
 
-	}
+            MatchSchedulerImpl ms = new MatchSchedulerImpl(new MaxSizeByeResolutionStrategy());
+            List<Match> matches = ms.schedule(mc);
+    
+            List<Match> finalMatches = MatchAnnealer.anneal(matches, 30);
+        
+            for (MatchSerializer serializer : serializers) {
+                    serializer.serialize(finalMatches, Team.generateTeamList(finalMatches));
+            }
+
+        }
+
+    }
 
 }
